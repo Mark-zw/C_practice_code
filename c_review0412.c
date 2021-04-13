6330a908c69f5f234d59893cd6c0ad43c9f809b0
zhangwu1997 <79700308+zhangwu1997@users.noreply.github.com>
2021-04-13 14:50:38 +0800

    Add files via upload

--- c_review0412.c
+++ c_review0412.c
@@ -1,26 +1,68 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-static int add(int x, int y)
-{
-	return x + y;
-}
-int main()
-{
-	int a = 10, b = 20;
-	int sum = add(a, b);
-	printf("%d", sum);
-}
-
 //int main()
 //{
-//	int include = 3;
-//	printf("%d", include);
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("休息日\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//		break;
+//	}
 //	return 0;
 //}
 //int main()
 //{
-//	int a = 1, b = 2, c = 3, d;
-//	d = (a + b, b++, c = b + a);
-//	printf("d = %d,c = %d\n", d, c);
+//	int i = 0;
+//	for (i = 1; i <= 100; i += 2)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num % 2 == 1)//判断奇数的方法：%2 看余数是否为1
+//		printf("%d是奇数\n", num);
+//	else
+//		printf("%d不是奇数\n", num);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//char c = 'a';
+//	//signed c1 = 'b';
+//	//unsigned c2 = 'c';
+//	//printf("%c %c %c", c, c1, c2);
+//	int arr1[10] = { 0 };//arr1的类型是int [10]，内存占40字节的代码块
+//	int arr2[20] = { 0 };//arr2的类型是int [20]，内存占80字节的代码块
+//
 //	return 0;
 //}
