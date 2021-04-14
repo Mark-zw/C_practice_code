2eebe832bbe8b19b3f68ce33689524a6229d59b1
zhangwu1997 <79700308+zhangwu1997@users.noreply.github.com>
2021-04-14 12:48:41 +0800

    Add files via upload

---
+++ c_review0413.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期三\n");
+//		break;
+//	case 4:
+//		printf("星期四\n");
+//		break;
+//	case 5:
+//		printf("星期五\n");
+//		break;
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期天\n"); 
+//		break;
+//	default:
+//		printf("输入错误\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 140;
+//	if (age < 18)
+//		printf("young pepole\n");//少年
+//	else if (age >= 18 && age < 26)
+//		printf("youth\n");//青年
+//	else if (age >= 26 && age < 50)
+//		printf("wrinkly\n");//中年
+//	else if (age >= 50 && age < 100)
+//		printf("older pepole\n");//老年
+//	else
+//		printf("the god of longevity!\n");//寿星
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 10;
+//	if (age >= 18)
+//		printf("adult\n");//成年
+//	else
+//		printf("nonage\n");//未成年
+//}
