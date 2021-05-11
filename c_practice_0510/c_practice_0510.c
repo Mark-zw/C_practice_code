1d7bdbcf682885201046e4ebabccdc3de143b794
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-05-11 17:36:22 +0800

    Add files via upload

--- c_practice_0510/c_practice_0510.c
+++ c_practice_0510/c_practice_0510.c
@@ -1,40 +1,88 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
 #include <stdio.h>
-#include <stdio.h>
-
-#define MAXN 10
-typedef float ElementType;
-
-ElementType Max(ElementType S[], int N);
-
+#include<stdio.h>
 int main()
 {
-    ElementType S[MAXN];
-    int N, i;
-
-    scanf("%d", &N);
-    for (i = 0; i < N; i++)
-        scanf("%f", &S[i]);
-    printf("%.2f\n", Max(S, N));
-
-    return 0;
+	void swap(int* p1, int* p2);
+	int a, b;
+	int* pointer_1, * pointer_2;
+	printf("piease enter a and b:");
+	scanf("%d,%d", &a, &b);
+	pointer_1 = &a;
+	pointer_2 = &b;
+	if (a < b) swap(pointer_1, pointer_2);
+	printf("max=%d,min=%d\n", a, b);
+	return 0;
 }
-
-/* 你的代码将被嵌在这里 */
-ElementType Max(ElementType S[], int N)
+void swap(int* p1, int* p2)
 {
-    int i = 0;
-    ElementType max = 0;
-    max = S[0];
-    for (i = 1; i < N; i++)
-    {
-        if (S[i]- S[i - 1] > 0)
-        {
-            max = S[i];
-        }
-    }
-    return max;
+	int temp;
+	temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
 }
+
+//printN打印1-N的数字
+//void printN(int n)//循环实现
+//{
+//	int i = 0;
+//	for (i = 0; i <= n; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//}
+//void printN(int n)//递归实现
+//{
+//	if (n)
+//	{
+//		printN(n - 1);
+//	}
+//	printf("%d ", n);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printN(n);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//#define MAXN 10
+//typedef float ElementType;
+//
+//ElementType Max(ElementType S[], int N);
+//
+//int main()
+//{
+//    ElementType S[MAXN];
+//    int N, i;
+//
+//    scanf("%d", &N);
+//    for (i = 0; i < N; i++)
+//        scanf("%f", &S[i]);
+//    printf("%.2f\n", Max(S, N));
+//
+//    return 0;
+//}
+//
+///* 你的代码将被嵌在这里 */
+//ElementType Max(ElementType S[], int N)
+//{
+//    int i = 0;
+//    ElementType max = 0;
+//    max = S[0];
+//    for (i = 1; i < N; i++)
+//    {
+//        if (S[i]- S[i - 1] > 0)
+//        {
+//            max = S[i];
+//        }
+//    }
+//    return max;
+//}
 
 
 //#define MAXN 10
