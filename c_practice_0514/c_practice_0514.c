ad8c9eefa1af5e165f80f7e7de3de23b968884b5
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-05-15 17:10:58 +0800

    Add files via upload

---
+++ c_practice_0514/c_practice_0514.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	char* p = "abcdef";
+//	printf("%c\n", *p);
+//	printf("%s\n", p);
+//	return 0;
+//}
+
+//void test(int arr[])
+//{
+//	int k = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", k);
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	test(arr);
+//	return 0;
+//}
