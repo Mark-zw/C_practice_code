12bde8295fac0ad7f58f438a35ccc11724187bb7
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-05-19 21:05:46 +0800

    Add files via upload

--- c_practice0518/c_practice0518_test.c
+++ c_practice0518/c_practice0518_test.c
@@ -1,12 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
 #include<stdio.h>
-int main()
+
+main()
+
 {
-	int i = 0;
-	if (1 || i++)
-	{
-		;
-	}
-	printf("%d", i);
-	return 0;
+	int x = 2, y = -1, z = 5;
+
+	if (x < y)
+
+		if (y < 0)
+
+			z = 0;
+
+		else
+
+			z = z + 1;
+
+	printf("%d\n", z);
+
 }
+
+//int main()
+//{
+//	int i = 0;
+//	if (1 || i++)
+//	{
+//		;
+//	}
+//	printf("%d", i);
+//	return 0;
+//}
