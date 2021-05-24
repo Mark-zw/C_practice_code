12e5f3c637fbbbd898b7681e18cbd1c8d09169df
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-05-24 22:51:44 +0800

    Add files via upload

---
+++ file_open/file_open.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a = 10000;
+	FILE* pf = fopen("test1.txt", "wb");
+	fwrite(&a, 4, 1, pf);
+	fclose(pf);
+	pf = NULL;
+	return 0;
+	return 0;
+}
