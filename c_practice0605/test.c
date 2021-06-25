f0da4f928e6484297b1d1a11f1f1367dde9e049b
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-06-25 13:47:03 +0800

    Add files via upload

---
+++ c_practice0605/test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+//int main()
+//{
+//	int line = 0;
+//	while (scanf("%d", &line) != EOF)
+//	{
+//		getchar();//讲多余的空格读走
+//
+//	}
+//	return 0;
+//}
