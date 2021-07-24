9dcdec2fe8ff85b8e3e6d3ce93fc83275680d059
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-07-24 10:46:59 +0800

    Add files via upload

---
+++ Contact_1/test.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include"contact.h"
+void menu()
+{
+	printf("************************************\n");
+	printf("******  1.Add       2.Del     ******\n");
+	printf("******  3.Search    4.Modify  ******\n");
+	printf("******  5.Sort      6.Print   ******\n");
+	printf("******        0.exit          ******\n");
+	printf("************************************\n");
+}
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SORT,
+	PRINT
+};
+int main()
+{
+	int input = 0;
+	Contact con;
+	InitContact(&con);//初始化
+	do
+	{
+		menu();
+		printf("please choose:>>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			//增加一个联系人
+			AddContact(&con);
+			break;
+		case DEL:
+			//删除一个联系人
+			DelContact(&con);
+			break; 
+		case SEARCH:
+			//查找一个联系人---根据姓名查找
+			SearchContact(&con);
+			break;
+		case MODIFY:
+			//修改一个联系人--根据姓名修改
+			ModifyContact(&con);
+			break;
+		case SORT:
+			//排序---按照年龄
+			SortContact(&con);
+			break;
+		case PRINT:
+			//打印
+			PrintContact(&con);
+			break;
+		case EXIT:
+			//退出
+			break;
+		default:
+			printf("choose wrong,please try again!\n");
+		}
+	} while (input);
+	return 0;
+}
