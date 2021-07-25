a52fcc522eb0cc87b37e9d2e21c076c466f0654d
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-07-25 10:27:09 +0800

    Add files via upload

--- contact.h
+++ contact.h
@@ -1,24 +1,43 @@
+#pragma once
 #define _CRT_SECURE_NO_WARNINGS 1 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-//静态的版本
+////静态的版本
+//
+//typedef struct PeoInfo//存放个人信息
+//{
+//	char name[20];//姓名
+//	short age;//年龄
+//	char sex[7];//性别
+//	char tele[12];//电话
+//	char address[20];//地址
+//}PeoInfo;
+//
+//#define MAX_SIZE 100
+////创建一个通讯录
+//typedef struct Contact
+//{
+//	PeoInfo data[MAX_SIZE];//存放100个人信息的数组
+//	int size;//表示当前存储个数
+//}Contact;
 
+//动态版本
 typedef struct PeoInfo//存放个人信息
 {
 	char name[20];//姓名
-	short age;//年龄
+	int age;//年龄
 	char sex[7];//性别
 	char tele[12];//电话
 	char address[20];//地址
 }PeoInfo;
 
-#define MAX_SIZE 100
-//创建一个通讯录
+#define INIT_SIZE 1 //供初始化使用
 typedef struct Contact
 {
-	PeoInfo data[MAX_SIZE];//存放100个人信息的数组
+	PeoInfo* data;//指向动态开辟数组的指针
 	int size;//表示当前存储个数
+	int capacity;//表示存储容量
 }Contact;
 
 void InitContact(Contact* pc);//初始化
@@ -34,3 +53,7 @@ void SearchContact(Contact* pc);
 void ModifyContact(Contact* pc);
 //排序---按照年龄
 void SortContact(Contact* pc);
+//进行扩容检查并扩容
+void CheckCapacity(Contact* pc);
+//销毁通讯录
+void DestoryContact(Contact* pc);
